28c2a2ffd8ab0b67513c123b7db44fa6f0dfecea
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-08-10 16:05:42 +0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,75 @@
+"use client";
+
 import Image from "next/image";
+import { useTimer } from "react-timer-hook";
 
 export default function Home() {
+  const { seconds, minutes, hours, days, isRunning } = useTimer({ expiryTimestamp: new Date("2024-10-26 18:00") });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-screen flex items-center justify-center bg-no-repeat bg-cover">
+      <Image src="/images/bg.png" alt="wedding card" fill className="object-cover" />
+      <div className="absolute top-0 left-0 size-full bg-black bg-opacity-10 flex items-center justify-center px-4">
+        <div className="relative h-[600px] w-[600px] min-w-[385px] bg-white bg-opacity-90 p-5 py-10 text-center flex flex-col align-center justify-between">
+          <div className="flex justify-center gap-3">
+            <Image src="/icons/rings.svg" alt="wedding card" width={50} height={50} />
+            <p className="text-3xl my-5 montserrat-400 grey-qo-regular">Taklifnoma</p>
+          </div>
+          <div>
+            <p className="text-3xl">Hurmatli mehmon</p>
+            <p className="text-xl my-5 montserrat-400 px-10">
+              Sizni{" "}
+              <span className="whitespace-nowrap">
+                <span className="grey-qo-regular text-2xl text-yellow-600">Xolmurod</span> va{" "}
+                <span className="grey-qo-regular text-2xl text-yellow-600">Marjonaxon</span>
+              </span>{" "}
+              larning nikoh to'yining faxriy mehmoni bo'lishga taklif qilamiz
+            </p>
+          </div>
+          <div>
+            <div>
+              <span className="font-medium">Boshlanish vaqti: </span>
+              <span className="text-gray-600 whitespace-nowrap">26-oktyabr, 2024-yil 18:00</span>
+            </div>
+            <div className="mb-2">
+              <span className="font-medium">Manzil: </span>
+              <span className="text-gray-600">Buxoro viloyati, Qorako'l tumani, O'zbegim to'yxonasi</span>
+            </div>
+            <a
+              href="https://maps.app.goo.gl/XBWCj59HXWhKpa5s6"
+              target="_blank"
+              className="underline mt-2 flex text-center justify-center"
+            >
+              <Image src="/icons/location.svg" alt="wedding card" width={24} height={24} />
+              Manzilni xaritada ko'rish
+            </a>
+          </div>
+          {isRunning ? (
+            <div className="text-center">
+              <p className="mb-2 font-medium">Marosim boshlanishiga qoldi:</p>
+              <div className="flex justify-center w-1/2 mx-auto">
+                <div className="flex flex-col grow w-fit text-gray-600">
+                  <span className="text-xl">{days}</span>
+                  <span>kun</span>
+                </div>
+                <div className="flex flex-col grow w-fit text-gray-600">
+                  <span className="text-xl">{hours}</span>
+                  <span>soat</span>
+                </div>
+                <div className="flex flex-col grow w-fit text-gray-600">
+                  <span className="text-xl">{minutes}</span>
+                  <span>daqiqa</span>
+                </div>
+                <div className="flex flex-col grow w-fit text-gray-600">
+                  <span className="text-xl">{seconds}</span>
+                  <span>soniya</span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p>Marosim boshlandi!</p>
+          )}
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
